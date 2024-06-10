d607336aa375498e4c0fdfbb53b5c6bd003bd4a4
alientheeight <czapski.tomasz@hotmail.com>
2024-06-10 22:42:10 +0200

    process.env.PORT feature added

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config()
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
